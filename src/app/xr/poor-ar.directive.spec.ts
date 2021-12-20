0634de365b8869d95ab39ad41da006cea07d2dca
Fulva1230 <b10503137@gapps.ntust.edu.tw>
2021-12-20 16:31:29 +0800

    commit losing component

---
+++ src/app/xr/poor-ar.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PoorArDirective } from './poor-ar.directive';
+
+describe('PoorArDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PoorArDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/xr/poor-ar.directive.ts
@@ -0,0 +1,95 @@
+import {Directive, ElementRef, OnInit} from '@angular/core';
+import * as THREE from "three";
+import {ARButton} from "three/examples/jsm/webxr/ARButton";
+import {TubePainter} from "three/examples/jsm/misc/TubePainter";
+
+@Directive({
+  selector: '[appPoorAr]'
+})
+export class PoorArDirective implements OnInit {
+
+  constructor(private el: ElementRef) {
+  }
+
+  ngOnInit(): void {
+    let container;
+    let camera: THREE.PerspectiveCamera, scene: THREE.Scene, renderer: THREE.WebGLRenderer;
+    let controller: THREE.Group, painter: any;
+
+    const cursor = new THREE.Vector3();
+
+    const init = () => {
+      container = document.createElement('div');
+      this.el.nativeElement.appendChild(container);
+
+      scene = new THREE.Scene();
+
+      camera = new THREE.PerspectiveCamera(70, this.el.nativeElement.clientWidth / this.el.nativeElement.clientHeight, 0.01, 20);
+
+      renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(this.el.nativeElement.clientWidth, this.el.nativeElement.clientHeight);
+      renderer.xr.enabled = true;
+      container.appendChild(renderer.domElement);
+      this.el.nativeElement.appendChild(ARButton.createButton(renderer));
+
+
+      const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+      light.position.set(0, 1, 0);
+      scene.add(light);
+
+      painter = new TubePainter();
+      painter.setSize(0.4);
+      painter.mesh.material.side = THREE.DoubleSide;
+      scene.add(painter.mesh);
+
+      function onSelectStart(this: THREE.Group) {
+        this.userData["isSelecting"] = true;
+        this.userData["skipFrames"] = 2;
+      }
+
+      function onSelectEnd(this: THREE.Group) {
+        this.userData["isSelecting"] = false;
+      }
+
+      controller = renderer.xr.getController(0);
+      controller.addEventListener('selectstart', onSelectStart);
+      controller.addEventListener('selectend', onSelectEnd);
+      controller.userData["skipFrames"] = 0;
+      scene.add(controller);
+    }
+
+    function handleController(controller: THREE.Group) {
+
+      const userData = controller.userData;
+
+      cursor.set(0, 0, -0.2).applyMatrix4(controller.matrixWorld);
+
+      if (userData['isSelecting'] === true) {
+
+        if (userData['skipFrames'] >= 0) {
+          userData['skipFrames']--;
+          painter.moveTo(cursor);
+
+        } else {
+          painter.lineTo(cursor);
+          painter.update();
+        }
+
+      }
+
+    }
+
+    function animate() {
+      renderer.setAnimationLoop(render);
+    }
+
+    function render() {
+      handleController(controller);
+      renderer.render(scene, camera);
+    }
+
+    init();
+    animate();
+  }
+}
