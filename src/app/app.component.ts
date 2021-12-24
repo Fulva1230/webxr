eb93b803268a8bb7b7238480adbdc0f8bb8211e5
Fulva1230 <b10503137@gapps.ntust.edu.tw>
2021-12-24 13:21:29 +0800

    add nebular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NbMenuItem, NbMenuService} from "@nebular/theme";
+import {filter} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'webxr';
+  items: NbMenuItem[] = [{ title: 'Business card' , link: '/model-view'}];
+
+  constructor() {
+  }
 }
